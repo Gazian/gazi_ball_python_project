07b02cf96304b643d48ae81e12de72926db51eeb
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2021-11-07 19:44:56 +0000

    committing with populated db

--- db/gazi_ball_functions.sql
+++ db/gazi_ball_functions.sql
@@ -7,3 +7,24 @@
     ON      t.league_id = l.id
     JOIN    stadiums s
     ON      t.stadium_id = s.id;
+
+    SELECT  m.season,
+            m.match_week,
+            m.match_date,
+            m.match_time,
+            l.league_name,
+            s.stadium_name,
+            home_team.team_name,
+            away_team.team_name,
+            m.home_score,
+            m.away_score
+    FROM    matches m         
+    JOIN    leagues l
+    ON      m.league_id = l.id
+    JOIN    teams home_team
+    ON      m.home_id = home_team.id
+    JOIN    teams away_team
+    ON      m.away_id = away_team.id
+    JOIN    stadiums s
+    ON      m.stadium_id = s.id;  
+
